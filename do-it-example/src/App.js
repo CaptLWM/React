05e2dc8882ed0f99dd767284e596afc07f5f65f2
CaptLWM <wonmin275@gmail.com>
2021-12-15 17:27:48 +0900

    211215 react

--- do-it-example/src/App.js
+++ do-it-example/src/App.js
@@ -1,8 +1,25 @@
-import React from "react";
-import MyComponent from "./01/RCC";
+import React from 'react';
+//import './App.css';
+//import MyComponent from "./01/RCC";
+//import MyComponent from './01/App';
+//import MyComponent from './03/TodaysPlan';
+//import MyComponent from './03/JSXSample';
+//import MyComponent from './03/PropsParentComponent';
+//import MyComponent from './03/PropsComponent';
+//import MyComponent from './03/ChildComponentExample';
+import MyComponent from './03/ChildComponentApp';
+//import MyComponent from './03/BooleanPropExample';
+//import MyComponent from './03/DefaultPropsExample';
+//import MyComponent from './03/ChildPropertyApp';
+//import MyComponent from './03/StateExampleApp';
+//import MyComponent from './03/ForceUpdateExampleApp';
+//import MyComponent from './03/LifecycleExampleApp';
+//import MyComponent from './03/CounterExample';
+//import MyComponent from './03/ListExample';
 
 class App extends React.Component {
   render() {
+    //화면 구성
     return (
       <div className="body">
         <MyComponent />
