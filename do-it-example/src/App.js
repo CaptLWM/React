d2c36a54374984f52c5375160e6578aef98eb16c
CaptLWM <wonmin275@gmail.com>
2021-12-15 12:58:20 +0900

    211215 react

---
+++ do-it-example/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MyComponent from "./01/RCC";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="body">
+        <MyComponent />
+      </div>
+    );
+  }
+}
+
+export default App;
