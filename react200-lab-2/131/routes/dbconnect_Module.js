738a3f016fdc99d242cded0c3c543843a8bc8d7e
CaptLWM <wonmin275@gmail.com>
2021-12-22 17:21:05 +0900

    211222 react

--- react200-lab-2/131/routes/dbconnect_Module.js
+++ react200-lab-2/131/routes/dbconnect_Module.js
@@ -6,14 +6,14 @@ const bodyParser = require("body-parser");
 router.use(bodyParser.json());
 
 // Connection Pool 세팅
-const pool  = mysql.createPool({
+const pool = mysql.createPool({
   connectionLimit: 66,
   waitForConnections: true,
-  host: "react200.cinvalghkckt.ap-northeast-2.rds.amazonaws.com",
+  host: "localhost",
   port: "3306",
-  database: 'react',
-  user: "admin",
-  password: "react200RDS",
+  database: "react",
+  user: "react",
+  password: "react",
 });
 
 router.post("/", (req, res) => {
@@ -21,42 +21,49 @@ router.post("/", (req, res) => {
   var param = req.body;
 
   //mybatis mapper경로 설정
-  mybatisMapper.createMapper(['./models/'+param.mapper+'.xml']);
+  mybatisMapper.createMapper(["./models/" + param.mapper + ".xml"]);
   var time = new Date();
-  console.log('## '+time+ ' ##');
-  console.log("\n Called Mapper Name  = "+param.mapper);
+  console.log("## " + time + " ##");
+  console.log("\n Called Mapper Name  = " + param.mapper);
 
-  var format = { language: 'sql', indent: '  ' };
+  var format = { language: "sql", indent: "  " };
   //mysql 쿼리 정보 세팅
-  var query = mybatisMapper.getStatement(param.mapper, param.mapper_id, param, format);
+  var query = mybatisMapper.getStatement(
+    param.mapper,
+    param.mapper_id,
+    param,
+    format
+  );
   console.log("\n========= Node Mybatis Query Log Start =========");
-  console.log("* mapper namespce : "+param.mapper+"."+param.mapper_id+" *\n");
-  console.log(query+"\n");
+  console.log(
+    "* mapper namespce : " + param.mapper + "." + param.mapper_id + " *\n"
+  );
+  console.log(query + "\n");
 
-  pool.getConnection(function(err,connection){
+  pool.getConnection(function (err, connection) {
     connection.query(query, function (error, results) {
       if (error) {
-        console.log("db error************* : "+error);
+        console.log("db error************* : " + error);
       }
       var time2 = new Date();
-      console.log('## '+time2+ ' ##');
-      console.log('## RESULT DATA LIST ## : \n', results);
-      if(results != undefined){
+      console.log("## " + time2 + " ##");
+      console.log("## RESULT DATA LIST ## : \n", results);
+      if (results != undefined) {
         string = JSON.stringify(results);
         var json = JSON.parse(string);
         if (req.body.crud == "select") {
           res.send({ json });
-        }else{
+        } else {
           res.send("succ");
         }
-      }else{
+      } else {
         res.send("error");
       }
 
       connection.release();
       console.log("========= Node Mybatis Query Log End =========\n");
     });
-  })
+  });
 });
 
 module.exports = router;
