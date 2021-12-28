1bbbb5f429f12627fec7d46eead75c6eb2c1f99f
CaptLWM <wonmin275@gmail.com>
2021-12-29 01:04:35 +0900

    211229 React 영화 웹 서비스 만들기 실습
    useEffect
    cleanup

--- reactWeb/myapp/src/App.js
+++ reactWeb/myapp/src/App.js
@@ -1,11 +1,26 @@
-import Button from "./Button.js";
-import styles from "./App.module.css";
+//import Button from "./Button.js";
+//import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+
+function Hello() {
+  function byeFn() {
+    console.log("bye :<");
+  }
+  function hiFn() {
+    console.log("hi :)");
+    return byeFn; // cleanup Function
+  }
+  useEffect(hiFn, []);
+  return <h1>hello</h1>;
+}
 
 function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back react!</h1>
-      <Button text={"Continue"} />
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
