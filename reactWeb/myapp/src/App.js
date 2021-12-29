084d9d41062ce395e198303046bd2419970dcdd8
CaptLWM <wonmin275@gmail.com>
2021-12-29 17:23:28 +0900

    211229 React 영화 웹 서비스 만들기 실습
    To Do list
    coin Tracker
    Movie App
    React Router
    Parameters
    publishing

--- reactWeb/myapp/src/App.js
+++ reactWeb/myapp/src/App.js
@@ -1,27 +1,24 @@
-//import Button from "./Button.js";
-//import styles from "./App.module.css";
-import { useState, useEffect } from "react";
-
-function Hello() {
-  function byeFn() {
-    console.log("bye :<");
-  }
-  function hiFn() {
-    console.log("hi :)");
-    return byeFn; // cleanup Function
-  }
-  useEffect(hiFn, []);
-  return <h1>hello</h1>;
-}
+//App.js 는 render router
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+//Link : 새로고침 없이 페이지 연결시켜줌
+import Home from "./routes/Home"; //연결시켜줄 페이지 import 필수
+import Detail from "./routes/Details";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
   return (
-    <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/hello">
+          <h2>Hello</h2>
+        </Route>
+        <Route path="/movie/:id">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
